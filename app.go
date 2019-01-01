125268a08c8ada699e179999e366949b1ccde400
ironpup <christoph.ding@gmail.com>
2019-01-01 14:06:07 -0500

    Stub endpoints related to plans.

--- app.go
+++ app.go
@@ -2,12 +2,44 @@ package main
 
 import (
   "fmt"
+  "net/http"
   // "log"
-  // "net/http"
 
   // "github.com/gorilla/mux"
 )
 
+// Endpoints by feature
+// I omit adding the descriptor 'handler' or 'endpoint' here because I thought 
+// that it adds unneccesary visual noise - it should be clear by how these functions
+// are used that they are handlers for requests
+
+// Plans
+func plansAll(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "getting all plans")
+}
+
+func plansByMarket(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "getting all plans by market")
+}
+
+func plansByID(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "getting all plans by id")
+}
+
+func plansCreate(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "creating plan")
+}
+
+func plansUpdate(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "updating plan")
+}
+
+func plansDelete(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "deleting plan")
+}
+
+// create server
 func main() {
-  fmt.Println("Hello world!")
+  fmt.Println("creating server ...")
+
 }
